14236c49c8630fc2d1e6d61d0de678b34520e9e9
Charpell <charpellumeh@gmail.com>
2018-08-08 23:50:22 +0100

    Horizontal Scroll with React Native Elements

--- App.js
+++ App.js
@@ -1,34 +1,76 @@
-import React, { Component } from 'react';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native';
+import { Card } from 'react-native-elements';
 
-export default class AnatomyExample extends Component {
+const data = [
+  {
+    imageUrl: "http://via.placeholder.com/160x160",
+    title: "something"
+  },
+  {
+    imageUrl: "http://via.placeholder.com/160x160",
+    title: "something two"
+  },
+  {
+    imageUrl: "http://via.placeholder.com/160x160",
+    title: "something three"
+  },
+  {
+    imageUrl: "http://via.placeholder.com/160x160",
+    title: "something four"
+  },
+  {
+    imageUrl: "http://via.placeholder.com/160x160",
+    title: "something five"
+  },
+  {
+    imageUrl: "http://via.placeholder.com/160x160",
+    title: "something six"
+  }
+];
+
+
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: data
+    };
+  }
+
   render() {
     return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <Text>
-            This is Content Section
-          </Text>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+      <FlatList
+        horizontal
+        data={this.state.data}
+        renderItem={({item: rowData}) => {
+          return(
+            <Card
+              style={{height: 200}}
+              title={null}
+              image={{url: rowData.imageUrl }}
+              containerStyle={{padding: 0, width: 160, height: 200, marginTop: 150}}
+              >
+              <Text style={{marginBottom: 10}}>{rowData.title}</Text>
+            </Card>
+          );
+        }
+    }
+    keyExtractor={(item, index) => index}
+      />
+
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
