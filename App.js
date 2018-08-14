f9cc9b140593c15f221775818eb9a625bc00db1a
Charpell <charpellumeh@gmail.com>
2018-08-14 11:55:28 +0100

    CheckList Component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ChatList from "./components/ChatList";
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Boiler Plate</Text>
+        <ChatList />
       </View>
     );
   }
@@ -13,9 +15,6 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#33ff62',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
